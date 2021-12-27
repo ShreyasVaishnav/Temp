edc84101f6b8290f8d88bc48723d92b1e558edc9
ShreyasVaishnav <83199543+ShreyasVaishnav@users.noreply.github.com>
2021-12-27 16:31:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,3 @@ import streamlit as st
 import pandas as pd
 
 st.header("Hello World!")
-
-ON_HEROKU = os.environ.get('ON_HEROKU')
-
-if ON_HEROKU:
-    # get the heroku port
-    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
-else:
-    port = 3000
-
-# functions that run and return output
